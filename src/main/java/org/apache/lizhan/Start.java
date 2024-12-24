89c247f56a1b6d038adc3b787e8956cbc3b21354
Lizhan54 <806312754@qq.com>
2024-12-24 20:16:51 +0800

    [测试提交]

---
+++ src/main/java/org/apache/lizhan/Start.java
@@ -0,0 +1,13 @@
+package org.apache.lizhan;
+
+/**
+ * @Author lz
+ * @company JYYH
+ * @Date 2024/12/24 20:15
+ * @Description TODO
+ * @packageName org.apache.lizhan
+ * @className: Start
+ * @Version 1.0
+ */
+public class Start {
+}
